4ed47971903831fe1cd65e14b693909411419920
zhanghongying <904047276@qq.com>
2026-03-23 08:08:42 +0800

    2nd commit

---
+++ prodigal.sh
@@ -0,0 +1,1 @@
+/ifs1/Software/miniforge3/envs/genome/bin/prodigal -a ref.fna.a.pep -d ref.fna.a.cds -f gff -g 11 -o ref.fna.a.gff -p single -s ref.fna.a.stat -i ref.fna 1>prodigal.log 2>prodigal.err
